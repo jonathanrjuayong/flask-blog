3b5a2393eba64c240df3c9c2e0c416f370b95278
JonathanJuayong <jonathan.juayong@gmail.com>
2018-03-02 17:13:46 +0800

    modified controller and template html

--- blog.py
+++ blog.py
@@ -6,6 +6,9 @@ from flask import Flask, render_template, request, session, \
     
 #configuration
 DATABASE = "blog.db"
+USERNAME = "admin"
+PASSWORD = "admin"
+SECRET_KEY = "hard_to_guess"
 
 app = Flask(__name__)
 
@@ -16,13 +19,29 @@ app.config.from_object(__name__)
 def connect_db ():
 	return sqlite3.connect(app.config["DATABASE"])
 	
-@app.route("/")
+@app.route("/", methods = ["GET","POST"])
 def login():
-	return render_template("login.html")
+	error = none
+	status_code = 200
+	if request.method == "POST":
+		if request.form["username"] != app.config["USERNAME"] or \
+		        request.form["password"] != app.config["PASSWORD"]:
+			error = "Invalid Credentials. Please try again."
+			status_code = 401
+		else:
+			session["logged_in"] = True
+			return redirect(url_for("main"))
+	return render_template("login.html", error=error), status_code
 	
 @app.route("/main")
 def main():
 	return render_template("main.html")
+	
+@app.route("/logout")
+def log_out():
+	session.pop("logged in", None)
+	flash("You were logged out")
+	return redirect_url(url_for("login"))
 
 if __name__ == "__main__":
 	app.run(debug = True)
